8df16b53b24645ca8e2544a8aa82cac964172065
Randall Rushing <randall.rushing@gmail.com>
2013-01-21 02:00:35 -0800

    CRT screen on/off animations user defined (1 of 2)
    Users can dynamically enable or disable CRT Screen Off or the
    elusive CRT Screen On animation.
    
    {Maybe i'll re-work the logic for aosp use, but so far, this works fine for now. }
    
    Change-Id: If9e1052555e1fdbcc1c718c61b5972932b2d48fa

--- services/java/com/android/server/power/DisplayPowerController.java
+++ services/java/com/android/server/power/DisplayPowerController.java
@@ -226,6 +226,10 @@ final class DisplayPowerController {
     // a stylish electron beam animation instead.
     private boolean mElectronBeamFadesConfig;
 
+    // Eos settings - override config for ElectronBeam on or off
+    private boolean mElectronBeamOnEnabled;
+    private boolean mElectronBeamOffEnabled;
+
     // The pending power request.
     // Initially null until the first call to requestPowerState.
     // Guarded by mLock.
@@ -661,6 +665,10 @@ final class DisplayPowerController {
             mustNotify = !mDisplayReadyLocked;
         }
 
+        // update crt settings here, it's only two bools
+        mElectronBeamOnEnabled = mPowerRequest.electronBeamOnEnabled;
+        mElectronBeamOffEnabled = mPowerRequest.electronBeamOffEnabled;
+
         // Initialize things the first time the power state is changed.
         if (mustInitialize) {
             initialize();
@@ -753,12 +761,12 @@ final class DisplayPowerController {
                         blockScreenOn();
                     } else {
                         unblockScreenOn();
-                        if (USE_ELECTRON_BEAM_ON_ANIMATION) {
+                        if (mElectronBeamOffEnabled) {
                             if (!mElectronBeamOnAnimator.isStarted()) {
                                 if (mPowerState.getElectronBeamLevel() == 1.0f) {
                                     mPowerState.dismissElectronBeam();
                                 } else if (mPowerState.prepareElectronBeam(
-                                        mElectronBeamFadesConfig ?
+                                        !mElectronBeamOnEnabled ?
                                                 ElectronBeam.MODE_FADE :
                                                         ElectronBeam.MODE_WARM_UP)) {
                                     mElectronBeamOnAnimator.start();
@@ -780,7 +788,7 @@ final class DisplayPowerController {
                         if (mPowerState.getElectronBeamLevel() == 0.0f) {
                             setScreenOn(false);
                         } else if (mPowerState.prepareElectronBeam(
-                                mElectronBeamFadesConfig ?
+                                !mElectronBeamOffEnabled ?
                                         ElectronBeam.MODE_FADE :
                                                 ElectronBeam.MODE_COOL_DOWN)
                                 && mPowerState.isScreenOn()) {
