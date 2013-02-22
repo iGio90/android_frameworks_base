7bc2fd0dfabe04c1a60e599aa49679ad8a358454
StevenHarperUK <stevenharperuk@gmail.com>
2013-02-22 23:06:14 +0000

    Bugfix : Quick pulldown sometimes flips when it should not
    When your Quick Settings Panel is set to
    
    Quick pulldown : right
    
    Sometimes (very intermittently) the tray flips (to Quick Settings) as you pull it down from the right side.
    
    This is happening because the pull down happened so fast you can see the top 0-10 pixels of the tray,
    and because it was showing the notifications, not the Quick Settiings
    
    However the user expects the tray not to flip, but to just be on the Quck Settings.
    
    The new code simply checks to see that the Event was a pull down event and that the event started in the top status bar.
    If this is the case, then the quick panel is swapped to (without flip)
    
    Special Thanks to my daughter Jenny fo painstakingly reproducing this bug.
    
    Change-Id: I8fdbedb1aa6d4fd672bd4cb58b7c99557469ab62
    
    Conflicts:
    
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -179,11 +179,33 @@ public class NotificationPanelView extends PanelView {
                     }
                     break;
             }
-            if(mOkToFlip && shouldFlip) {
-                if (getMeasuredHeight() < mHandleBarHeight) {
-                    mStatusBar.switchToSettings();
-                } else {
-                    mStatusBar.flipToSettings();
+            if (mOkToFlip && flip) {
+                float miny = event.getY(0);
+                float maxy = miny;
+                for (int i=1; i<event.getPointerCount(); i++) {
+                    final float y = event.getY(i);
+                    if (y < miny) miny = y;
+                    if (y > maxy) maxy = y;
+                }
+                if (maxy - miny < mHandleBarHeight) {
+                    if (getMeasuredHeight() < mHandleBarHeight) {
+                        mStatusBar.switchToSettings();
+                    } else {
+                        // Do not flip if the drag event started within the top bar
+                        if (MotionEvent.ACTION_DOWN == event.getActionMasked() && event.getY(0) < mHandleBarHeight ) {
+                            mStatusBar.switchToSettings();
+                        } else {
+                            mStatusBar.flipToSettings();
+                        }
+                    }
+                    mOkToFlip = false;
+                }
+            } else if (mSwipeTriggered) {
+                final float deltaX = (event.getX(0) - mGestureStartX) * mSwipeDirection;
+                mStatusBar.partialFlip(mFlipOffset +
+                                       deltaX / (getWidth() * STATUS_BAR_SWIPE_MOVE_PERCENTAGE));
+                if (!swipeFlipJustStarted) {
+                    return true; // Consume the event.
                 }
                 mOkToFlip = false;
             }
