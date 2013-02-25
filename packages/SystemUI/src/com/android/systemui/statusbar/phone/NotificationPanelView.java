004757c349717f43187bdc74ce3130b3e8587da0
igio90 <giovanni.rocca.90@gmail.com>
2013-02-25 09:32:09 +0100

    another one

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -187,7 +187,6 @@ public class NotificationPanelView extends PanelView {
 		}
                 mOkToFlip = false;
             }
-        }
         return mHandleView.dispatchTouchEvent(event);
     }
 }
