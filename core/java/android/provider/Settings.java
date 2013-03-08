2153f65b951671832a278134ff7739bd6cacfba6
Bajee11 <bajee11@gmail.com>
2013-03-07 21:27:58 -0500

    MMS Breath (2/3)
    This will give your SMS notifications a breathing effect in the statusbar instead of being a static icon.
    
    Conflicts:
    
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3816,6 +3816,11 @@ public final class Settings {
         public static final String MUTE_ANNOYING_NOTIFICATIONS_THRESHOLD = "mute_annoying_notifications_threshold";
 
         /**
+         * @hide
+         */
+        public static final String MMS_BREATH = "mms_breath";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
