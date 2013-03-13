c2de9bb59d51b85936569618f339994729137cd4
igio90 <giovanni.rocca.90@gmail.com>
2013-03-13 16:34:58 +0100

    Port "Option to control cursor in text fields using volume keys (1/2)"

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3278,6 +3278,15 @@ public final class Settings {
          */
         public static final String UI_FORCE_OVERFLOW_BUTTON = "ui_force_overflow_button";
 
+         /**
+          * Volume keys control cursor in text fields (default is 0)
+          * 0 - Disabled
+          * 1 - Volume up/down moves cursor left/right
+          * 2 - Volume up/down moves cursor right/left
+          * @hide
+          */
+         public static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
+
         /**
          * AM/PM Style for clock options
          * 0 - Normal AM/PM
