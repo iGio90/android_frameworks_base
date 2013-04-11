84cbb6aaa0050957b7cd5d3a45b04f8f91391ec0
Thomas Wendt <thoemy@gmx.net>
2013-04-12 00:51:36 +0200

    Revert "Add Facebook contact support (2/2)"
    This reverts commit 042f39ef0e006b0df1e0bfec5ac54905814adfcc.

--- core/java/android/provider/ContactsContract.java
+++ core/java/android/provider/ContactsContract.java
@@ -752,8 +752,6 @@ public final class ContactsContract {
         public static final String SYNC3 = "sync3";
         /** Generic column for use by sync adapters. */
         public static final String SYNC4 = "sync4";
-        /** Facebook */
-        public static final String IS_RESTRICTED = "is_restricted";
     }
 
     /**
