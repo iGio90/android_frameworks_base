2bc29e6f0de7a98676290304d3877d9a1fe4f680
igio90 <giovanni.rocca.90@gmail.com>
2013-04-15 19:10:37 +0200

    OpenPdroid 5/5 Signed-off-by: igio90 <giovanni.rocca.90@gmail.com>

--- telephony/java/android/telephony/ServiceState.java
+++ telephony/java/android/telephony/ServiceState.java
@@ -487,7 +487,10 @@ public class ServiceState implements Parcelable {
                 + " EmergOnly=" + mIsEmergencyOnly);
     }
 
-    private void setNullState(int state) {
+   // BEGIN privacy-modified
+   // Was originally private
+   void setNullState(int state) {
+   // END privacy-modified
         mState = state;
         mRoaming = false;
         mOperatorAlphaLong = null;
