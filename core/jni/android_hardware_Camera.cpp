0c3d7f31d8fe1004cd25be4307a1b0c2829918b2
Steve Kondik <shade@chemlab.org>
2013-05-04 03:07:51 -0700

    Camera: add support for cm camera
    camera: Add support for sending raw commands
    
     * Certain camera drivers need magic commands to select special modes
       such as ZSL or HDR. Add support for sending raw commands from
       applications.
    
    Change-Id: I512a765c7a67ffd2877e465cf6493ffc2b3b54ac
    
    Camera: add ISO mode support
    
    Change-Id: Ied2075f950269990d836fc768b2c6c36088d494f
    
    Camera: add additional ISO modes (2/2)
    
    Change-Id: I3460c3de4cbef467430673146a9ddb2fdc8f5032
    
    Camera : Powerkey shutter (1/2)
    
    Allows user to take use power key to take pictures/videos
    
    Change-Id: I85a1e1761199f4604672be42a3a5005227f5451a
    
    Conflicts:
    
    	core/java/android/view/WindowManager.java
    
    Signed-off-by: igio90 <giovanni.rocca.90@gmail.com>

--- core/jni/android_hardware_Camera.cpp
+++ core/jni/android_hardware_Camera.cpp
@@ -863,6 +863,17 @@ static void android_hardware_Camera_sendRawCommand(JNIEnv *env, jobject thiz, ji
     }
 }
 
+static void android_hardware_Camera_sendRawCommand(JNIEnv *env, jobject thiz, jint arg1, jint arg2, jint arg3)
+{
+    ALOGV("sendRawCommand %d, %d, %d", arg1, arg2, arg3);
+    sp<Camera> camera = get_native_camera(env, thiz, NULL);
+    if (camera == 0) return;
+
+    if (camera->sendCommand(arg1, arg2, arg3) != NO_ERROR) {
+        jniThrowRuntimeException(env, "send raw command failed");
+    }
+}
+
 //-------------------------------------------------
 
 static JNINativeMethod camMethods[] = {
