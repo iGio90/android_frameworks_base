898e6599b044a6e09b8959cab3b551238b8a35da
drcmda <drcmda@gmail.com>
2013-05-04 02:41:46 +0100

    fixes
    Change-Id: I9e7090ecad797a6775d3440991f40d8027578207

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -1340,7 +1340,7 @@ public abstract class BaseStatusBar extends SystemUI implements
         private String mPkg;
         private String mTag;
         private int mId;
-        private boolean mFloat = true;
+        private boolean mFloat;
 
         NotificationClicker(PendingIntent intent, String pkg, String tag, int id) {
             mIntent = intent;
@@ -1374,7 +1374,6 @@ public abstract class BaseStatusBar extends SystemUI implements
                 overlay.setSourceBounds(
                         new Rect(pos[0], pos[1], pos[0]+v.getWidth(), pos[1]+v.getHeight()));
                 try {
-                    android.util.Log.d("PARANOID", "sending intent="+mIntent + " multi="+mFloat);
                     mIntent.send(mContext, 0, overlay);                    
                 } catch (PendingIntent.CanceledException e) {
                     // the stack trace isn't very helpful here.  Just log the exception message.
