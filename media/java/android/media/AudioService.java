58d4b9378b1ce9c686121a33fe62d95b6c3030e3
igio90 <giovanni.rocca.90@gmail.com>
2013-05-11 18:15:58 +0200

    audio: Add overlay to fix analog docks.
    Of particular interest to devices using libhardware_legacy and
    affected by commit 5a484b753cc72d6a50c1dd3bbf68b3403c741a3a: http://goo.gl/rVR1Z
    which restricted the routing of AUDIO_DEVICE_OUT_ANLG_DOCK_HEADSET
    to docks using FORCE_ANALOG_DOCK cases. The EXTRA_DOCK_STATE_CAR case is
    returning FORCE_BT_CAR_DOCK, preventing the dock audio from routing properly.
    Add an overlay option for overriding to FORCE_ANALOG_DOCK.
    Change-Id: Ib7d70c242aa6f537ea5d65098ceb2c591662d8bb
    
    Signed-off-by: igio90 <giovanni.rocca.90@gmail.com>

--- media/java/android/media/AudioService.java
+++ media/java/android/media/AudioService.java
@@ -446,6 +446,9 @@ public class AudioService extends IAudioService.Stub implements OnFinished {
 
     private boolean mDockAudioMediaEnabled = true;
 
+    private boolean mForceAnalogDeskDock;
+    private boolean mForceAnalogCarDock;
+
     private int mDockState = Intent.EXTRA_DOCK_STATE_UNDOCKED;
 
     ///////////////////////////////////////////////////////////////////////////
@@ -566,6 +569,12 @@ public class AudioService extends IAudioService.Stub implements OnFinished {
 
         mMasterVolumeRamp = context.getResources().getIntArray(
                 com.android.internal.R.array.config_masterVolumeRamp);
+
+        mForceAnalogDeskDock = mContext.getResources().getBoolean(
+                com.android.internal.R.bool.config_forceAnalogDeskDock);
+
+        mForceAnalogCarDock = mContext.getResources().getBoolean(
+                com.android.internal.R.bool.config_forceAnalogCarDock);
 
         mMainRemote = new RemotePlaybackState(-1, MAX_STREAM_VOLUME[AudioManager.STREAM_MUSIC],
                 MAX_STREAM_VOLUME[AudioManager.STREAM_MUSIC]);
@@ -3871,10 +3880,10 @@ public class AudioService extends IAudioService.Stub implements OnFinished {
                 int config;
                 switch (dockState) {
                     case Intent.EXTRA_DOCK_STATE_DESK:
-                        config = AudioSystem.FORCE_BT_DESK_DOCK;
+			config = mForceAnalogDeskDock ? AudioSystem.FORCE_ANALOG_DOCK : AudioSystem.FORCE_BT_DESK_DOCK;
                         break;
                     case Intent.EXTRA_DOCK_STATE_CAR:
-                        config = AudioSystem.FORCE_BT_CAR_DOCK;
+                        config = mForceAnalogCarDock ? AudioSystem.FORCE_ANALOG_DOCK : AudioSystem.FORCE_BT_CAR_DOCK;
                         break;
                     case Intent.EXTRA_DOCK_STATE_LE_DESK:
                         config = AudioSystem.FORCE_ANALOG_DOCK;
