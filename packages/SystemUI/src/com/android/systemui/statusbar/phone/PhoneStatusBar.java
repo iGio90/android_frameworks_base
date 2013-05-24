8387d034730458c504038e790f6cf5fdae083ed9
Lars Greiss <kufikugel@googlemail.com>
2013-05-24 10:16:26 +0200

    Frameworks: TRDS 2.0 (1/3)
    After testing with huge success we discovered here
    and there small problems (like NFC beam animation is broken,
    TRDS was disabled on cardock etc) which mainly was caused
    due that we used the already from AOSP introduced uiModes
    qualifier which could cause in special conditions this
    conflicts. Beside that we want a bit more flexibility.
    
    Result: This commit introduces the inverted qualifier as
    complete own qualifier undependent of the already AOSP
    used ones.
    
    Beside that it fixes all known issues......features:
    
    - it is now completly working over the AOSP Configuration classes
      Means every app get notice of a change trough onConfiguratioChange
      and restarts it's activity if change was detected.
    - app can disable that like some other configuration values
      over AndroidMainfest.xml
    - beside "inverted" folder another "notinverted" folder is introduced
      for another themeing (eg white)
    - qualifier order is direct before the official uiMode qualifiers
      (see AOSP docs)
    - All usual Configuration actions can be used with the inverted
      qualifiers....like BroadcastChange, direct read out from every
      app etc. etc. Note: Only system can change the uiInvertedMode
    
    So as you see a real own qualifier now
    
    NOTE: Apktool as well aapt need to be changed to fit the "new
    qualifier"....which means that we need to recompile our gapps
    that they react as well on this new behaviour. Think we will get them ready
    next 2 or 3 days.
    
    Patchset:
    increase support from SDK_HONEYCOMB_MR2
    to FROYO to have better compatibillity
    to some gapps.
    
    Change-Id: I851740010198c400266d7ca9995eb08dbf7782bc
    
    Conflicts:
    
    	packages/SystemUI/src/com/android/systemui/statusbar/WidgetView.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
    
    Signed-off-by: igio90 <giovanni.rocca.90@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -195,9 +195,8 @@ public class PhoneStatusBar extends BaseStatusBar {
     int mIconHPadding = -1;
     Display mDisplay;
     Point mCurrentDisplaySize = new Point();
-    int mCurrentUIMode = 0; 
-
     private boolean mHasDockBattery;
+    int mCurrUiInvertedMode;
 
     IDreamManager mDreamManager;
 
@@ -235,7 +234,6 @@ public class PhoneStatusBar extends BaseStatusBar {
 
     // settings
     QuickSettingsController mQS;
-    boolean mUiModeIsToggled;
     boolean mHasSettingsPanel, mHideSettingsPanel, mHasFlipSettings;
     SettingsPanelView mSettingsPanel;
     View mFlipSettingsView;
@@ -414,6 +412,8 @@ public class PhoneStatusBar extends BaseStatusBar {
         mDreamManager = IDreamManager.Stub.asInterface(
                 ServiceManager.checkService(DreamService.DREAM_SERVICE));
 
+        mCurrUiInvertedMode = mContext.getResources().getConfiguration().uiInvertedMode;
+
         CustomTheme currentTheme = mContext.getResources().getConfiguration().customTheme;
         if (currentTheme != null) {
             mCurrentTheme = (CustomTheme)currentTheme.clone();
@@ -566,9 +566,6 @@ public class PhoneStatusBar extends BaseStatusBar {
         mClearButton.setVisibility(View.INVISIBLE);
         mClearButton.setEnabled(false);
         mDateView = (DateView)mStatusBarWindow.findViewById(R.id.date);
-
-        mUiModeIsToggled = Settings.Secure.getInt(mContext.getContentResolver(),
-                              Settings.Secure.UI_MODE_IS_TOGGLED, 0) == 1;
 
         if (mStatusBarView.hasFullWidthNotifications()) {
             mHideSettingsPanel = Settings.System.getInt(mContext.getContentResolver(),
@@ -2990,11 +2987,20 @@ public class PhoneStatusBar extends BaseStatusBar {
         final Context context = mContext;
         final Resources res = context.getResources();
 
+        // detect inverted ui mode change
+        int uiInvertedMode =
+            mContext.getResources().getConfiguration().uiInvertedMode;
+
         // detect theme change.
         CustomTheme newTheme = res.getConfiguration().customTheme;
-        if (newTheme != null &&
-                (mCurrentTheme == null || !mCurrentTheme.equals(newTheme))) {
-            mCurrentTheme = (CustomTheme)newTheme.clone();
+        if ((newTheme != null &&
+                (mCurrentTheme == null || !mCurrentTheme.equals(newTheme)))
+            || uiInvertedMode != mCurrUiInvertedMode) {
+            if (uiInvertedMode != mCurrUiInvertedMode) {
+                mCurrUiInvertedMode = uiInvertedMode;
+            } else {
+                mCurrentTheme = (CustomTheme) newTheme.clone();
+            }
             recreateStatusBar();
         } else {
 
@@ -3175,11 +3181,8 @@ public class PhoneStatusBar extends BaseStatusBar {
         public void onChange(boolean selfChange) {
             boolean hideSettingsPanel = Settings.System.getInt(mContext.getContentResolver(),
                                     Settings.System.QS_DISABLE_PANEL, 0) == 1;
-            boolean uiModeIsToggled = Settings.Secure.getInt(mContext.getContentResolver(),
-                                    Settings.Secure.UI_MODE_IS_TOGGLED, 0) == 1;
 
-            if (hideSettingsPanel != mHideSettingsPanel
-                || uiModeIsToggled != mUiModeIsToggled) {
+            if (hideSettingsPanel != mHideSettingsPanel) {
                 recreateStatusBar();
             }
 
@@ -3257,10 +3260,6 @@ public class PhoneStatusBar extends BaseStatusBar {
 
         public void startObserving() {
             final ContentResolver cr = mContext.getContentResolver();
-
-            cr.registerContentObserver(
-                    Settings.Secure.getUriFor(Settings.Secure.UI_MODE_IS_TOGGLED),
-                    false, this);
 
             cr.registerContentObserver(
                     Settings.System.getUriFor(Settings.System.NOTIF_WALLPAPER_ALPHA),
