e0e0933ed263035650c42e9dd48f8710f335717c
igio90 <giovanni.rocca.90@gmail.com>
2013-05-29 23:58:50 +0200

    Revert "[1/2] Power connect/disconnect notification support"
    This reverts commit f8f9e2e58fe3f7a0c4a5cac0cd22db17397385b9.

--- packages/SystemUI/src/com/android/systemui/power/PowerUI.java
+++ packages/SystemUI/src/com/android/systemui/power/PowerUI.java
@@ -69,9 +69,6 @@ public class PowerUI extends SystemUI {
     AlertDialog mLowBatteryDialog;
     TextView mBatteryLevelTextView;
 
-    // For filtering ACTION_POWER_DISCONNECTED on boot
-    boolean mIgnoreFirstPowerEvent = true;
-
     public void start() {
 
         mLowBatteryAlertCloseLevel = mContext.getResources().getInteger(
@@ -90,7 +87,6 @@ public class PowerUI extends SystemUI {
         IntentFilter filter = new IntentFilter();
         filter.addAction(Intent.ACTION_BATTERY_CHANGED);
         filter.addAction(Intent.ACTION_POWER_CONNECTED);
-        filter.addAction(Intent.ACTION_POWER_DISCONNECTED);
         mContext.registerReceiver(mIntentReceiver, filter, null, mHandler);
     }
 
@@ -164,10 +160,6 @@ public class PowerUI extends SystemUI {
                 final boolean plugged = mPlugType != 0;
                 final boolean oldPlugged = oldPlugType != 0;
 
-                if (mIgnoreFirstPowerEvent && plugged) {
-                    mIgnoreFirstPowerEvent = false;
-                }
-
                 int oldBucket = findBatteryLevelBucket(oldBatteryLevel);
                 int bucket = findBatteryLevelBucket(mBatteryLevel);
 
@@ -209,18 +201,6 @@ public class PowerUI extends SystemUI {
                     dismissLowBatteryWarning();
                 } else if (mShowLowBatteryWarning && mBatteryLevelTextView != null) {
                     showLowBatteryWarning();
-                }
-            } else if (action.equals(Intent.ACTION_POWER_CONNECTED)
-                    || action.equals(Intent.ACTION_POWER_DISCONNECTED)) {
-                final ContentResolver cr = mContext.getContentResolver();
-
-                if (mIgnoreFirstPowerEvent) {
-                    mIgnoreFirstPowerEvent = false;
-                } else {
-                    if (Settings.Global.getInt(cr,
-                            Settings.Global.POWER_NOTIFICATIONS_ENABLED, 0) == 1) {
-                        playPowerNotificationSound();
-                    }
                 }
             } else {
                 Slog.w(TAG, "unknown intent: " + intent);
@@ -341,34 +321,6 @@ public class PowerUI extends SystemUI {
         d.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
         d.show();
         mInvalidChargerDialog = d;
-    }
-
-    void playPowerNotificationSound() {
-        final ContentResolver cr = mContext.getContentResolver();
-        final String soundPath =
-                Settings.Global.getString(cr, Settings.Global.POWER_NOTIFICATIONS_RINGTONE);
-
-        NotificationManager notificationManager =
-                (NotificationManager)mContext.getSystemService(Context.NOTIFICATION_SERVICE);
-        if (notificationManager == null) {
-            return;
-        }
-
-        Notification powerNotify=new Notification();
-        powerNotify.defaults = Notification.DEFAULT_ALL;
-        if (soundPath != null) {
-            powerNotify.sound = Uri.parse(soundPath);
-            if (powerNotify.sound != null) {
-                // DEFAULT_SOUND overrides so flip off
-                powerNotify.defaults &= ~Notification.DEFAULT_SOUND;
-            }
-        }
-        if (Settings.Global.getInt(cr,
-                Settings.Global.POWER_NOTIFICATIONS_VIBRATE, 0) == 0) {
-            powerNotify.defaults &= ~Notification.DEFAULT_VIBRATE;
-        }
-
-        notificationManager.notify(0, powerNotify);
     }
     
     public void dump(FileDescriptor fd, PrintWriter pw, String[] args) {
